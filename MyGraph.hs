7735e5bf51972183efbe570c5f697b6f75458447
Mikołaj Błaż <mb346862@students.mimuw.edu.pl>
2016-03-21 15:27:48 +0100

    Complete Graph

--- MyGraph.hs
+++ MyGraph.hs
@@ -1,15 +1,29 @@
-module MyGraph where
+module MyGraph(
+  Graph,
+  fromList, updateVertex, neighbours, dfs, dfsFrom
+) where
 
 import qualified Data.Set as Set
-import MyArray
+import qualified MyArray
 
 type VIx = Int
-tpye VData = [VIx]
+type VData = [VIx]
+type Graph = MyArray.Array VIx VData
 
-type Graph = Array VIx VData
+fromList :: [(VIx, VData)] -> Graph
+fromList = MyArray.array (0::VIx, maxBound::VIx)
 
 updateVertex :: VIx -> VData -> Graph -> Graph
-updateVertex = update
+updateVertex = MyArray.update
+
+neighbours :: Graph -> VIx -> [VIx]
+neighbours = (MyArray.!)
 
 dfs :: Graph -> [VIx]
-dfsFrom :: Graph -> VIx -> Set -> Set
+dfs g = Set.toAscList $ dfsFrom g 1 Set.empty
+
+dfsFrom :: Graph -> VIx -> Set.Set VIx -> Set.Set VIx
+dfsFrom g v visited = foldr action visited $ neighbours g v
+  where
+    action neigh visited | neigh `Set.member` visited = visited
+                         | otherwise = dfsFrom g neigh (Set.insert neigh visited)
