f8c426d142d48a247afbf9155e2e575ff17813be
Mikołaj Błaż <mb346862@students.mimuw.edu.pl>
2016-03-22 11:51:10 +0100

    Prawie gotowe, pozostaje konwersja na Inty

--- MyArray.hs
+++ MyArray.hs
@@ -20,48 +20,49 @@ data IntervalTree e =
 
 -- | Tworzy pusty węzeł z rekurencyjnie tworzonymi dziećmi
 makeEmpty :: (Int, Int) -> IntervalTree e
-makeEmpty (a, b) | a == b = EmptyLeaf
-                 | otherwise = EmptyBranch (a, b) (makeEmpty (a, s)) (makeEmpty (s + 1, b))
+makeEmpty (a, b) | a < b = EmptyBranch (a, b) (makeEmpty (a, s)) (makeEmpty (s + 1, b))
+                 | otherwise = EmptyLeaf
   where s = half (a, b)
 
 unsafeFind :: Int -> IntervalTree e -> IntervalTree e
 unsafeFind k (Branch ran l r) | k <= half ran = unsafeFind k l
                               | otherwise     = unsafeFind k r
-unsafeFind k n = n
+unsafeFind _ n = n
 
-contains  :: (Ix i) => i -> IntervalTree e -> Bool
-contains _ (EmptyBranch _ _ _) = False
-contains _ (EmptyLeaf) = False
-contains _ (Leaf _) = True
-contains k (Branch ran l r) | k <= half ran = contains k l
-                            | otherwise     = contains k r
+unsafeContains  :: Int -> IntervalTree e -> Bool
+unsafeContains k t = case unsafeFind k t of
+  Leaf _    -> True
+  otherwise -> False
 
-unsafeInsert :: (Ix i) => i -> e -> IntervalTree e -> IntervalTree e
+unsafeGetElem   :: Int -> IntervalTree e -> e
+unsafeGetElem k t = case unsafeFind k t of
+  Leaf v    -> v
+  otherwise -> error "No such index"
+
+unsafeInsert :: Int -> e -> IntervalTree e -> IntervalTree e
 unsafeInsert _ v EmptyLeaf = Leaf v
-unsafeInsert _ v (Leaf vn) = Leaf v
+unsafeInsert _ v (Leaf _)  = Leaf v
 unsafeInsert k v (EmptyBranch ran l r) | k <= half ran = Branch ran (unsafeInsert k v l) r
                                        | otherwise     = Branch ran l (unsafeInsert k v r)
 unsafeInsert k v (Branch ran l r)      | k <= half ran = Branch ran (unsafeInsert k v l) r
                                        | otherwise     = Branch ran l (unsafeInsert k v r)
 
-insert :: (Ix i) => i -> e -> IntervalTree e -> IntervalTree e
-insert k v t = undefined
+insert :: Int -> e -> IntervalTree e -> IntervalTree e
+insert _ v EmptyLeaf = Leaf v
+insert _ v (Leaf _)  = Leaf v
+insert k v t@(EmptyBranch ran _ _) | inRange ran k = unsafeInsert k v t
+                                   | otherwise     = error "Index out of range"
+insert k v t@(Branch ran l r)      | inRange ran k = unsafeInsert k v t
+                                   | otherwise     = error "Index out of range"
 
-getElem   :: (Ix i) => i -> IntervalTree e -> e
-getElem _ EmptyLeaf           = error "No such index"
-getElem _ (EmptyBranch _ _ _) = error "No such index"
-getElem _ (Leaf v) = v
-getElem k (Branch ran l r) | k <= half ran = getElem k l
-                           | otherwise     = getElem k r
-
-fromList  :: (Ix i) => (i, i) -> [(i, e)] -> IntervalTree e
+fromList  :: (Int, Int) -> [(Int, e)] -> IntervalTree e
 fromList ran kvs = foldr (\(k, v) tree -> insert k v tree) (makeEmpty ran) kvs
 
-toList    :: (Ix i) => IntervalTree e -> [e]
+toList    :: IntervalTree e -> [e]
 toList EmptyLeaf           = []
 toList (EmptyBranch _ _ _) = []
 toList (Leaf v) = [v]
-toList (Branch _ vn l r) = (toList l) ++ (vn : toList r)
+toList (Branch _ l r) = (toList l) ++ (toList r)
 
 
 
@@ -78,7 +79,7 @@ listArray r es = array r (zip (range r) es)
 
 -- | Daje element tablicy o podanym indeksie
 (!)       :: Ix i => Array i e -> i -> e
-(!) (Arr r t) k | inRange r k = getElem k t
+(!) (Arr r t) k | inRange r k = unsafeGetElem k t
                 | otherwise = error "Index out of range"
 
 -- | Daje listę elementów tablicy (w kolejności indeksów)
@@ -87,11 +88,11 @@ elems (Arr _ t) = toList t
 
 -- | Buduje tablicę z podanej listy par (indeks,wartość)
 array     :: (Ix i) => (i, i) -> [(i,e)] -> Array i e
-array r kvs = Arr r (fromList kvs)
+array r kvs = Arr r (fromList r kvs)
 
 -- | Daje tablicę będącą wariantem danej, zmienioną pod podanym indeksem
 update    :: Ix i => i -> e -> Array i e -> Array i e
-update k v (Arr r t) | inRange r k = Arr r (insert k v t)
+update k v (Arr r t) | inRange r k = Arr r (unsafeInsert k v t)
                      | otherwise = error "Index out of range"
 
 -- | Daje tablicę będącą wariantem danej, zmienioną pod podanymi indeksami
@@ -100,4 +101,5 @@ update k v (Arr r t) | inRange r k = Arr r (insert k v t)
 
 -- | Sprawdza czy w tablicy znajduje się coś pod danym indeksem
 present   :: Ix i => i -> Array i e -> Bool
-present k (Arr _ t) = contains k t
+present k (Arr r t) | inRange r k = unsafeContains k t
+                    | otherwise   = False
