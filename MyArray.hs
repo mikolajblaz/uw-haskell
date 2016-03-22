6f701d037fa56aa19eda651ec34b9e2c55b1842f
Mikołaj Błaż <mb346862@students.mimuw.edu.pl>
2016-03-22 11:21:23 +0100

    Pełne drzewo przedizałowe

--- MyArray.hs
+++ MyArray.hs
@@ -6,61 +6,64 @@ module MyArray(
 
 import MyIndex
 
-half :: Int -> Int -> Int
-half a b = (a + b) `div` 2
+half :: (Int, Int) -> Int
+half (a, b) = (a + b) `div` 2
 
 -- Typ drzew przedziałowych wraz z podstawową implementacją
 data IntervalTree e =
     Branch      (Int, Int) (IntervalTree e) (IntervalTree e) |
     EmptyBranch (Int, Int) (IntervalTree e) (IntervalTree e) |
-    Leaf        Int e |
-    EmptyLeaf   Int
+    Leaf e |
+    EmptyLeaf
   deriving (Eq, Show)
 
 
 -- | Tworzy pusty węzeł z rekurencyjnie tworzonymi dziećmi
 makeEmpty :: (Int, Int) -> IntervalTree e
-makeEmpty (a, b) | a == b = EmptyLeaf a
+makeEmpty (a, b) | a == b = EmptyLeaf
                  | otherwise = EmptyBranch (a, b) (makeEmpty (a, s)) (makeEmpty (s + 1, b))
-  where s = half a b
+  where s = half (a, b)
 
 unsafeFind :: Int -> IntervalTree e -> IntervalTree e
-unsafeFind k (Branch (a, b) l r) | k <= s = unsafeFind k l
-                                 | otherwise = unsafeFind k r
-  where s = half a b
+unsafeFind k (Branch ran l r) | k <= half ran = unsafeFind k l
+                              | otherwise     = unsafeFind k r
 unsafeFind k n = n
 
 contains  :: (Ix i) => i -> IntervalTree e -> Bool
 contains _ (EmptyBranch _ _ _) = False
-contains _ (EmptyLeaf _) = False
-contains _ (Leaf _ _) = True
-contains k (Branch (a, b) l r) | k <= s = contains k l
-                               | otherwise = contains k r
-  where s = half a b
+contains _ (EmptyLeaf) = False
+contains _ (Leaf _) = True
+contains k (Branch ran l r) | k <= half ran = contains k l
+                            | otherwise     = contains k r
 
---------- TODO: poniżej
+unsafeInsert :: (Ix i) => i -> e -> IntervalTree e -> IntervalTree e
+unsafeInsert _ v EmptyLeaf = Leaf v
+unsafeInsert _ v (Leaf vn) = Leaf v
+unsafeInsert k v (EmptyBranch ran l r) | k <= half ran = Branch ran (unsafeInsert k v l) r
+                                       | otherwise     = Branch ran l (unsafeInsert k v r)
+unsafeInsert k v (Branch ran l r)      | k <= half ran = Branch ran (unsafeInsert k v l) r
+                                       | otherwise     = Branch ran l (unsafeInsert k v r)
 
-insert    :: (Ix i) => i -> e -> IntervalTree e -> IntervalTree e
-insert k v Empty = Branch k v Empty Empty
-insert k v (Branch (a, b) l r) | k < kn = Branch kn vn (insert k v l) r
-                            | k > kn = Branch kn vn l (insert k v r)
-                            | otherwise = Branch kn v l r
+insert :: (Ix i) => i -> e -> IntervalTree e -> IntervalTree e
+insert k v t = undefined
 
 getElem   :: (Ix i) => i -> IntervalTree e -> e
-getElem _ Empty = error "No such index"
-getElem k (Branch (a, b) l r) | k < kn = getElem k l
-                           | k > kn = getElem k r
-                           | otherwise = vn
+getElem _ EmptyLeaf           = error "No such index"
+getElem _ (EmptyBranch _ _ _) = error "No such index"
+getElem _ (Leaf v) = v
+getElem k (Branch ran l r) | k <= half ran = getElem k l
+                           | otherwise     = getElem k r
 
-fromList  :: (Ix i) => [(i, e)] -> IntervalTree e
-fromList kvs = foldr (\(k, v) tree -> insert k v tree) Empty kvs
+fromList  :: (Ix i) => (i, i) -> [(i, e)] -> IntervalTree e
+fromList ran kvs = foldr (\(k, v) tree -> insert k v tree) (makeEmpty ran) kvs
 
 toList    :: (Ix i) => IntervalTree e -> [e]
-toList Empty = []
+toList EmptyLeaf           = []
+toList (EmptyBranch _ _ _) = []
+toList (Leaf v) = [v]
 toList (Branch _ vn l r) = (toList l) ++ (vn : toList r)
 
 
------------ TODO: powyżej
 
 -- Tablice
 data Array i e = Arr {rng :: (i, i), tree :: (IntervalTree e)}
