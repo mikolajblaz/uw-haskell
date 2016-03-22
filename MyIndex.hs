164f20c92fcad4d2e5f4c96fc4bb6d1ecf855ae7
Mikołaj Błaż <mb346862@students.mimuw.edu.pl>
2016-03-22 21:07:36 +0100

    Powrót MyIndex

--- MyIndex.hs
+++ MyIndex.hs
@@ -18,7 +18,7 @@ class Ord a => Ix a where
 instance Ix Char where
   range (a, b) = [a..b]
   index (a, b) c | inRange (a, b) c = fromEnum c - fromEnum a
-                 | otherwise = error ("Index " ++ show c ++ " out of range " ++ show (a, b))
+                 | otherwise = error "Index out of range"
   inRange (a, b) c = a <= c && c <= b
   rangeSize (a, b) | a <= b = fromEnum b - fromEnum a + 1
                    | otherwise = 0
@@ -27,7 +27,7 @@ instance Ix Char where
 instance Ix Int where
   range (a, b) = [a..b]
   index (a, b) c | inRange (a, b) c = c - a
-                 | otherwise = error ("Index " ++ show c ++ " out of range " ++ show (a, b))
+                 | otherwise = error "Index out of range"
   inRange (a, b) c = a <= c && c <= b
   rangeSize (a, b) | a <= b = b - a + 1
                    | otherwise = 0
@@ -35,7 +35,7 @@ instance Ix Int where
 instance Ix Integer where
   range (a, b) = [a..b]
   index (a, b) c | inRange (a, b) c = fromInteger (c - a)
-                 | otherwise = error "[Integer] Index out of range"
+                 | otherwise = error "Index out of range"
   inRange (a, b) c = a <= c && c <= b
   rangeSize (a, b) | a <= b = fromInteger (b - a + 1)
                    | otherwise = 0
@@ -44,6 +44,6 @@ instance (Ix a, Ix b) => Ix (a,b) where
   range ((a1, b1), (a2, b2)) = [(x, y) | x <- range (a1, a2), y <- range (b1, b2)]
   index ((a1, b1), (a2, b2)) (c, d)
     | inRange ((a1, b1), (a2, b2)) (c, d) = index (a1, a2) c * rangeSize (b1, b2) + index (b1, b2) d
-    | otherwise = error "[A, B] Index out of range"
+    | otherwise = error "Index out of range"
   inRange ((a1, b1), (a2, b2)) (c, d) = inRange (a1, a2) c && inRange (b1, b2) d
   rangeSize ((a1, b1), (a2, b2)) = rangeSize (a1, a2) * rangeSize (b1, b2)
